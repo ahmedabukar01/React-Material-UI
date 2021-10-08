b8e10557c15ea983950fe449c8f6b69d709aa7ae
ahmed <ahmedlearn01@gmail.com>
2021-10-07 17:43:16 -0700

    material ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import {Typography, AppBar, Card, CardActions, CardMedia, CssBaseline, Grid, Toolbar,Container} from '@material-ui/core'
+import PhotoCamera from '@material-ui/icons'
 
 const App = () => {
   return (
-    <div>
+    <>
       
-    </div>
+    </>
   )
 }
 
