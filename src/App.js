8e33c1a1790b5e6e0a1e2401e855f762464d86c3
ahmed <ahmedlearn01@gmail.com>
2021-10-09 10:27:04 -0700

    different ins

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import {Typography, AppBar, Card, CardActions, CardMedia, CssBaseline, Grid, Toolbar,Container} from '@material-ui/core'
 import {PhotoCamera} from '@material-ui/icons'
 
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+
 const App = () => {
   return (
     <>
@@ -21,6 +24,16 @@ const App = () => {
             <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
               Photo Album
             </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              Hi Everyone here is photo album that i am creating so this text is just testing long paragraph so as always keep practicing.
+            </Typography>
+
+            <ButtonGroup variant="contained" aria-label="outlined primary button group">
+              <Button>One</Button>
+              <Button>Two</Button>
+              <Button>Three</Button>
+            </ButtonGroup>
+        
           </Container>
         </div>
         </main>
